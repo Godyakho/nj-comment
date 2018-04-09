6b052db2e0bd7a50a87871de8c97c8895e1be9e7
niuzz <niuzz@hotmail.com>
2018-04-09 17:03:51 +0800

    init

---
+++ comment-admin/src/components/login.vue
@@ -0,0 +1,19 @@
+<!-- ******************************************
+*  Author : niuzz niuzz@hotmail.com
+*  Created On : Mon Apr 09 2018
+******************************************* -->
+<template>
+  <div class="wrap">
+    <h1>
+      login
+    </h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
